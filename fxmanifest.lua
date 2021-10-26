bfea8687c8d46acf3ce133bff596f981081a80b0
Aux33 <73757442+Aux33@users.noreply.github.com>
2021-10-25 21:57:02 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+author 'Aux#6193'
+
+
+-- INCLUDED FILES
+
+files {
+	'weaponcomponents.meta',
+	'weaponanimations.meta',
+	'weapons.meta'
+	}
+
+-- DATA FILES
+
+data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'weaponcomponents.meta'
